600919d753df98b86921fa686595faba833994d8
jthoeun <johnny.thoeun@gmail.com>
2023-05-03 20:54:34 -0600

    Added code for Prepare activty week 3

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,26 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning02 World!");
+        static void Main(string[] args)
+        {
+            Job job1= new Job();
+            job1._jobTitle = "Quality Assurance Specialist";
+            job1._company = "SAS Retail Services";
+            job1._startYear = 2020;
+            job1._endYear = 2022;
+
+            Job job2 = new Job();
+            job2._jobTitle = "Product Lister & Shipping Manager";
+            job2._company = "eAsset Solutions LLC";
+            job2._startYear = 2016;
+            job2._endYear = 2019;
+
+            Resume myResume = new Resume();
+            myResume._name = "Johnny Thoeun";
+            myResume._jobs.Add(job1);
+            myResume._jobs.Add(job2);
+            myResume.Display();
+
+        
+        }
     }
-}
